275df6c51907cf2b8b5f65110481a63059ec0401
Arush Pande <asp.workmail97@gmail.com>
2026-02-20 13:31:16 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -48,8 +48,6 @@
  * The mainPage value must match a key in the PAGES object exactly.
  */
 import Activity from './pages/Activity';
-import Agents from './pages/Agents';
-import Console from './pages/Console';
 import CreateJob from './pages/CreateJob';
 import CreatePipeline from './pages/CreatePipeline';
 import Dashboard from './pages/Dashboard';
@@ -57,14 +55,14 @@ import Evaluation from './pages/Evaluation';
 import Inbox from './pages/Inbox';
 import JobDetails from './pages/JobDetails';
 import Settings from './pages/Settings';
+import Console from './pages/Console';
 import TalentPool from './pages/TalentPool';
+import Agents from './pages/Agents';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Activity": Activity,
-    "Agents": Agents,
-    "Console": Console,
     "CreateJob": CreateJob,
     "CreatePipeline": CreatePipeline,
     "Dashboard": Dashboard,
@@ -72,7 +70,9 @@ export const PAGES = {
     "Inbox": Inbox,
     "JobDetails": JobDetails,
     "Settings": Settings,
+    "Console": Console,
     "TalentPool": TalentPool,
+    "Agents": Agents,
 }
 
 export const pagesConfig = {
