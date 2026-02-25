bd96185ea74066cb6f666a24b2db7422b9b91d93
Arush Pande <asp.workmail97@gmail.com>
2026-02-25 12:06:42 +0000

    Manual code change

--- src/pages/CreateJob.jsx
+++ src/pages/CreateJob.jsx
@@ -226,7 +226,7 @@ export default function CreateJob() {
               <div className="space-y-6 max-w-6xl mx-auto">
                 <div className="bg-slate-50 p-8 rounded-2xl shadow-sm">
                   <div className="mb-6 pb-6 border-b border-gray-200">
-                    <h2 className="text-[28px] font-bold text-gray-900 mb-3">{generatedJob.title}</h2>
+                    <h2 className="text-gray-900 mb-3 text-base font-semibold">{generatedJob.title}</h2>
                     
 
 
