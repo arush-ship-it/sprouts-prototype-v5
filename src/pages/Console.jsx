50d476049eaa88521e4693688f67fe034ac078ab
Arush Pande <asp.workmail97@gmail.com>
2026-03-05 07:33:31 +0000

    Manual code change

--- src/pages/Console.jsx
+++ src/pages/Console.jsx
@@ -47,14 +47,14 @@ export default function Console() {
         <div className="px-6 flex items-center gap-2 justify-end">
                 <Button
             onClick={() => setShowSandboxBuilder(true)}
-            size="sm" className="bg-slate-100 text-slate-600 px-3 text-xs font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow hover:bg-indigo-700 h-8">
+            size="sm" className="bg-slate-100 text-slate-100 px-3 text-xs font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow hover:bg-indigo-700 h-8">
 
                   + Create New
                 </Button>
                 <Button
             onClick={() => setShowRecommendedPipeline(true)}
-            size="sm"
-            className="bg-slate-100 text-slate-700 px-3 font-medium rounded-md h-8 hover:bg-slate-200">
+            size="sm" className="bg-slate-100 text-slate-200 px-3 text-xs font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow h-8 hover:bg-slate-200">
+
                   <Workflow className="w-3.5 h-3.5 mr-1.5" />
                   Build Pipeline
                 </Button>
