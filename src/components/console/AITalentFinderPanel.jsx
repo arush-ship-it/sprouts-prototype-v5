8ac3cb2c9793e17a44714d3ebe4044e45f3de971
Arush Pande <asp.workmail97@gmail.com>
2026-03-05 13:05:59 +0000

    Manual code change

--- src/components/console/AITalentFinderPanel.jsx
+++ src/components/console/AITalentFinderPanel.jsx
@@ -46,9 +46,9 @@ export default function AITalentFinderPanel() {
   });
 
   return (
-    <div className="bg-white mx-2 my-2 rounded-2xl w-[483px] border-r border-gray-200 flex flex-col h-screen">
+    <div className="bg-white mx-4 my-2 rounded-2xl w-[483px] border-r border-gray-200 flex flex-col h-screen">
       {/* Top Tabs */}
-      <div className="px-3 py-3 border-b border-gray-200">
+      <div className="px-4 py-4 border-b border-gray-200">
         <div className="flex items-center justify-between mb-4">
           <div className="bg-slate-100 px-1 py-1 rounded-[10px] flex gap-1">
             <button
