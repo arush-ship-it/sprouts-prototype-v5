d804e19c924f0797d68a57c7f9437f9fde9f9507
Arush Pandya <asp.workmail97@gmail.com>
2026-03-06 18:52:20 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -226,7 +226,7 @@ export default function Home() {
       {/* Main 2-col layout */}
       <div className="flex gap-6 flex-1 min-h-0">
         {/* Left column */}
-        <div className="flex-1 min-w-0 flex flex-col gap-6">
+        <div className="flex-1 min-w-0 flex flex-col gap-6 overflow-y-auto">
           {/* 4 insight cards */}
           <div className="grid grid-cols-3 gap-4">
             {insightCards.map((card, i) => <InsightCard key={i} card={card} />)}
