1d54cad5f627710e1652a963c781ebe65a49252e
Arush Pandya <asp.workmail97@gmail.com>
2026-03-07 14:33:18 +0000

    File changes

--- src/components/console/ActivityApprovalModal.jsx
+++ src/components/console/ActivityApprovalModal.jsx
@@ -255,7 +255,6 @@ export default function ActivityApprovalModal({ isOpen, onClose }) {
               </div>
             </div>
           </div>
-        )}
       </DialogContent>
     </Dialog>
   );
