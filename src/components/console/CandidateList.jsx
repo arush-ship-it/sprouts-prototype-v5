9d10bcbebade23d3e2e46a3602c537a57bebf921
Arush Pandya <asp.workmail97@gmail.com>
2026-03-09 10:19:20 +0000

    File changes

--- src/components/console/CandidateList.jsx
+++ src/components/console/CandidateList.jsx
@@ -578,8 +578,7 @@ export default function CandidateList({ activeTab, viewMode = "card" }) {
                 </div>
           }
             </div>
-            </>}
-        }
+          }
         </div>
       }
 
